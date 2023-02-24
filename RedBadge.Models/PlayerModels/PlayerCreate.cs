ae4014c387f21ceef1a629ecf28fbeea910bbff0
Adam <Guy McGee>
2023-02-24 01:06:03 -0500

    Player Models complete

---
+++ RedBadge.Models/PlayerModels/PlayerCreate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBadge.Models.PlayerModels
+{
+    internal class PlayerCreate
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ RedBadge.Models/PlayerModels/PlayerEdit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBadge.Models.PlayerModels
+{
+    internal class PlayerEdit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
