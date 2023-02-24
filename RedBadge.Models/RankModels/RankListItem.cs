1db2b68bce75358357b2580b095c2768c5318540
Adam <Guy McGee>
2023-02-24 01:12:25 -0500

    Rank Models complete

---
+++ RedBadge.Models/RankModels/RankListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBadge.Models.RankModels
+{
+    internal class RankListItem
+    {
+        public int Id { get; set; }
+        public string RankName { get; set; }
+    }
+}
