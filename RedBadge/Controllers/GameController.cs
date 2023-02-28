2bde4b2a984675ccdccdb3e4ae722b4c9ac179ee
Adam <Guy McGee>
2023-02-28 03:06:13 -0500

    GaemCreate Endpoint complete

--- RedBadge/Controllers/GameController.cs
+++ RedBadge/Controllers/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RedBadge.Models.GameModels;
 
 namespace RedBadge.Controllers
 {
@@ -7,6 +8,26 @@ namespace RedBadge.Controllers
     [ApiController]
     public class GameController : ControllerBase
     {
-        private readonly IGameSErvice
+        private readonly IGameService _gameService;
+        public GameController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [HttpPost("Create Game")]
+        public async Task<IActionResult> CreateGame([FromBody] GameCreate gameModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var gameCreateResult = await _gameService.GameCreateAsync(gameModel);
+            if(gameCreateResult)
+            {
+                return Ok("Game was created");
+            }
+            return BadRequest("Game could not be created");
+        }
     }
 }
