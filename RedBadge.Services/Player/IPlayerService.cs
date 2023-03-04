7a2b48f5635cb457e99477f464d8b797e2d28872
Adam <Guy McGee>
2023-03-04 00:55:51 -0500

    IPlayer Service complete

--- RedBadge.Services/Player/IPlayerService.cs
+++ RedBadge.Services/Player/IPlayerService.cs
@@ -1,4 +1,14 @@
-﻿public interface IPlayerService
-{
+﻿using RedBadge.Models.PlayerModels;
 
+public interface IPlayerService
+{
+    public Task<bool> CreatePlayerAsync(PlayerCreate playerToCreate);
+
+    public Task<List<PlayerListItem>> GetAllPlayersAsync();
+
+    public Task<PlayerDetails> GetPlayerByIdAsync(int playerId);
+
+    public Task<bool> UpdatePlayerAsync(int playerId, PlayerEdit playerModel);
+
+    public Task<bool> DeletePlayerAsync(int playerId);
 }
