aebd2be442b8f78950577830ec399ea6f4f47b67
Adam <Guy McGee>
2023-03-12 01:06:14 -0500

    Game Controller Create new game functionality added

--- RedBadge/Controllers/GameController.cs
+++ RedBadge/Controllers/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RedBadge.Models.GameModels;
 using RedBadge.Services.Game;
 
 namespace RedBadge.Controllers
@@ -17,6 +18,25 @@ namespace RedBadge.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _gameService.GetAllGamesAsync());
+        }
+
+        [HttpGet]
+        [Route("Post")]
+        public async Task<IActionResult> Post()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Post")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Post(GameCreate game)
+        {
+            if(!ModelState.IsValid)return BadRequest(game);
+            if(await _gameService.CreateGameAsync(game))
+                return RedirectToAction(nameof(Index));
+            else
+                return View(game);
         }
     }
 }
