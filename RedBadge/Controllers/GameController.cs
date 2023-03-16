8f0965dc5bc1b4cd4b31885f2b8dbcb2247dd10f
Adam <Guy McGee>
2023-03-15 22:49:14 -0400

    FULL CRUD for Game View complete

--- RedBadge/Controllers/GameController.cs
+++ RedBadge/Controllers/GameController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using RedBadge.Models.GameModels;
+using RedBadge.Models.PlayerModels;
 using RedBadge.Services.Game;
+using RedBadge.Services.Player;
 
 namespace RedBadge.Controllers
 {
@@ -20,6 +23,12 @@ namespace RedBadge.Controllers
             return View(await _gameService.GetAllGamesAsync());
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await _gameService.GetGameByIdAsync(id));
+        }
+
         [HttpGet]
         [Route("Post")]
         public async Task<IActionResult> Post()
@@ -37,6 +46,52 @@ namespace RedBadge.Controllers
                 return RedirectToAction(nameof(Index));
             else
                 return View(game);
+        }
+
+        [HttpGet]
+        [Route("Edit/{id}")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var game = await _gameService.GetGameByIdAsync(id);
+            var gameEdit = new GameEdit
+            {
+                Id = game.Id,
+                Name = game.Name,
+            };
+            return View(gameEdit);
+        }
+
+        [HttpPost]
+        [Route("Edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, GameEdit gameModel)
+        {
+            if (id!=gameModel.Id) return BadRequest("Invalid ID");
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            if (await _gameService.UpdateGameAsync(id, gameModel))
+                return RedirectToAction(nameof(Index));
+            else
+                return View(gameModel);
+        }
+
+        [HttpGet]
+        [Route("Delete/{id}")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var game = await _gameService.GetGameByIdAsync(id.Value);
+            return View(game);
+        }
+
+        [HttpPost]
+        [Route("Delete/{id}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var isSuccessful = await _gameService.DeleteGameAsync(id);
+            if (isSuccessful)
+                return RedirectToAction(nameof(Index));
+            else
+                return UnprocessableEntity();
         }
     }
 }
